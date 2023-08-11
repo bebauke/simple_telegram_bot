bc89583143c786e436589baa7c1c46988c85522a
bebauke <45176812+bebauke@users.noreply.github.com>
2023-08-11 03:01:34 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="telegram_bot",
+    version="1.0",
+    author="Bernhard Bauke",
+    description="A simple Telegram Bot for Python",
+    packages=find_packages(),
+)
